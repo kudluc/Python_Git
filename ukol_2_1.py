87bfb8594a42322746826fed231e26c9cf103891
kudluc <kudluc@volny.cz>
2025-04-23 21:42:15 +0200

    ukol_2_2_v1

--- ukol_2_1.py
+++ ukol_2_1.py
@@ -1,19 +1,13 @@
 import json
 import requests
 
-ICO = int(input('Dobrý den! O kterém subjektu chcete získat informace? Zadejte jeho IČO:'))
+ico = input('Dobrý den! O kterém subjektu chcete získat informace? Zadejte jeho IČO: ')
 
-response = requests.get(f'https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/{ICO}')
+response = requests.get(f'https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/{ico}')
 data = response.json()
 
-with open('ICO.json', 'w', encoding='utf-8') as file:
-    json.dump(data, file, indent=2, ensure_ascii=False)
+informace_z_OR = data.get('ekonomickeSubjekty', {})
 
-with open('ICO.json', encoding='utf-8') as file:
-    informace_z_OR = json.load(file)
-    
-jmeno_subjektu = informace_z_OR['obchodniJmeno']
-adresa_subjeku = informace_z_OR['sidlo']['textovaAdresa']
-
-print(jmeno_subjektu)
-print(adresa_subjeku)
+  
+jmeno_subjektu = data.get('obchodniJmeno', 'Neznámý subjekt')
+adresa_subjektu = data.get('sidlo', {}).get('textovaAdresa', 'Neznámá adresa')
--- ukol_2_2.py
+++ ukol_2_2.py
@@ -1,32 +1,25 @@
 import requests
 import json
 
-subjekt = input('Dobrý den! Jaký je název subjektu, který chcete vyhledat? Zadejte jeho jméno:')
+subjekt = input('Dobrý den! Jaký je název subjektu, který chcete vyhledat? Zadejte jeho jméno: ')
 
 headers = {
     'accept': 'application/json',
     'Content-Type': 'application/json',
 }
 
+
 data = {'obchodniJmeno': subjekt}
 
 response = requests.post('https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/vyhledat', headers=headers, json=data)
 
-data = response.json()
+odpoved = response.json()
+pocet = odpoved.get('pocetCelkem', 0)
+subj_list = odpoved.get('ekonomickeSubjekty', {})
 
-with open('subj.json', 'w', encoding='utf-8') as file:
-    json.dump(data, file, indent=2, ensure_ascii=False)
+print(f'Nalezeno subjektů: {pocet}')
 
-with open('subj.json', encoding='utf-8') as file:
-    subj_list = json.load(file)
-
-subj_celkem = int(subj_list['pocetCelkem'])
-
-print(f'Nalezeno subjektů: {subj_celkem}')
-
-ekonomicke_subjekty = subj_list['ekonomickeSubjekty']
-
-for subjekt in ekonomicke_subjekty:
+for subjekt in subj_list:
     obchodni_jmeno = subjekt['obchodniJmeno']
     ico = subjekt['ico']
     
